b70eaebc85c1df5a713ce7699a79789376f22b81
Boothand <boothand@hotmail.com>
2016-09-01 02:54:59 +0200

    Converter works! Outputs: Amount of curves - "Curve " + index, number of vertices per curve, xyz rgb uv

--- main.cpp
+++ main.cpp
@@ -84,152 +84,178 @@ void ConvertToVerticesTest(string in = "../Oblig1/SOSI/in.sos", string out = "..
 void ConvertToCurves(string in = "../Oblig1/SOSI/in.sos", string out = "../Oblig1/SOSI/curves.txt")
 {
     ifstream reader;
-    ofstream writer;
-    
-    reader.open(in);
-    writer.open(out);
-    
-    float unit = 0.001f;
-    int curveIndex = 0;
-    int vertexIndex = 0;
-    int pass = 1;
-    bool readingCurves = false;
-    bool readingVerts = false;
-    bool foundStartCurve = false;
-    
-    int startCurveLineNum = 0;
-    
-    vector<int> vertexAmountArray;
-    
-    string str;
-    float x, y, z;
-    
-    enum State { countCurves, countVertices, write, done };
-    State stage = countCurves;
-    
-    if (reader.is_open())
-    {
-        while (!reader.eof() && stage != done)
+        ofstream writer;
+        
+        reader.open(in);
+        writer.open(out);
+        
+        int curveCount = 0;
+        bool readingVerts = false;
+        bool readingCurves = false;
+        float x, y, z;
+        vector<int> vertexAmountList;
+        
+        float unit = 0.001f;
+        
+        string str;        
+        
+        enum State { countCurves, countVertices, write, done };
+        State stage = countCurves;
+        
+        if (reader.is_open())
         {
-            getline(reader, str);
-            
-            
-            switch (stage)
+            while (stage != done)
             {
-            case countCurves:
-                
-                break;
-            case countVertices:
-                
-                break;
-                
-            case write:
-                
-                break;
-                
-            case done:
-                
-                break;
-            }
-        }
-    }
-    
-    return;
-    
-    
-    
-    
-    if (reader.is_open())
-    {
-        while (!reader.eof() && pass < 4)
-        {
-            getline(reader, str);
-            
-            if (!foundStartCurve)
-            {
-                startCurveLineNum++;
-            }
-            
-            if (strncmp(str.c_str(), ".SLUTT", 6) == 0)
-            {
-                if (pass == 3)
+                switch (stage)
                 {
-                    writer << curveIndex << endl;
-                    
-                }
-                reader.seekg(startCurveLineNum - 1, ios::beg);
-                pass++;
-                vertexIndex = 0;
-                curveIndex = 0;
-                readingCurves = false;
-                readingVerts = false;
-                
-                continue;
-            }
-            
-            if (strncmp(str.c_str(), ".KURVE", 6) == 0)
-            {
-                if (!foundStartCurve)
-                {
-                    foundStartCurve = true;
-                }
-                
-                curveIndex++;
-                if (pass > 1)
-                {
-                    writer << "Curve " << curveIndex << endl;
-                    writer << vertexIndex << endl;
-                }
-                
-                vertexIndex = 0;
-                readingVerts = false;
-                readingCurves = true;
-            }
-            
-            if (readingCurves)
-            {
-                if (strncmp(str.c_str(), "..HØYDE", 7) == 0)
-                {
-                    str = str.substr(8, (int)str.length() - 8);
-                    z = std::stof(str.c_str(), nullptr);
-                }
-                else if (strncmp(str.c_str(), "..NØ", 4) == 0)
-                {
-                    readingVerts = true;
-                    getline(reader, str);
-                }
-            
-                if (readingVerts)
-                {
-                    if (pass > 1)
+                    case countCurves:
                     {
-                        std:size_t splitPos = str.find(" ");
-                        string yStr = str.substr(0, (int)splitPos);
-                        string xStr = str.substr((int)splitPos + 1, (int)str.length() - (int)yStr.length());
-                        
-                        y = std::stof(yStr.c_str(), nullptr);
-                        x = std::stof(xStr.c_str(), nullptr);
-                        
-                        x = x * unit;
-                        y = y * unit;
-                        z = z * unit;                
-                        
-                        writer << "(" << x << ", " << y << ", " << z << ") (0.5, 0.5, 0.5) (1, 0)" << endl;
+                        while (!reader.eof())
+                        {
+                            getline(reader, str);
+                            
+                            if (strncmp(str.c_str(), ".KURVE", 6) == 0)
+                            {
+                                curveCount++;
+                            }
+                            
+                            if (strncmp(str.c_str(), ".SLUTT", 6) == 0)
+                            {
+                                reader.seekg(0, ios::beg);  //Read from beginning again.
+                                stage = countVertices;
+                                break;
+                            }
+                        }
+                        break;
                     }
-                    
-                    vertexIndex++;
+                    case countVertices:
+                    {
+                        while (!reader.eof())
+                        {
+                            getline(reader, str);
+                            
+                            if (strncmp(str.c_str(), ".KURVE", 6) == 0)
+                            {
+                                readingCurves = true;
+                            }
+                            
+                            if (readingCurves)
+                            {
+                                if (strncmp(str.c_str(), "..NØ", 4) == 0)
+                                {
+                                    readingVerts = true;
+                                    getline(reader, str);
+                                }
+                                
+                                if (readingVerts)
+                                {
+                                    int vertsPerCurve = 0;
+                                    while (true)
+                                    {
+                                        vertsPerCurve++;
+                                        getline(reader, str);
+                                        
+                                        if (strncmp(str.c_str(), ".KURVE", 6) == 0)
+                                        {
+                                            vertexAmountList.push_back(vertsPerCurve);
+                                            readingVerts = false;
+                                            break;
+                                        }
+                                        
+                                        if (strncmp(str.c_str(), ".SLUTT", 6) == 0)
+                                        {
+                                            vertexAmountList.push_back(vertsPerCurve);
+                                            readingVerts = false;
+                                            readingCurves = false;
+                                            
+                                            reader.seekg(0, ios::beg);  //Read from beginning again.
+                                            
+                                            stage = write;
+                                            break;
+                                        }
+                                    }
+                                    
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    }  
+                    case write:
+                    {
+                        int curveIndex = 0;
+                        writer << curveCount << endl;
+                        
+                        while (!reader.eof())
+                        {
+                            getline(reader, str);
+                            
+                            if (strncmp(str.c_str(), ".KURVE", 6) == 0)
+                            {
+                                readingCurves = true;
+                                readingVerts = false;
+                                
+                                curveIndex++;
+                                writer << "Curve " << curveIndex << endl;
+                                writer << vertexAmountList.front() << endl;
+                                vertexAmountList.erase(vertexAmountList.begin());
+                            }
+                            
+                            if (readingCurves && !readingVerts)
+                            {
+                                if (strncmp(str.c_str(), "..HØYDE", 7) == 0)
+                                {
+                                    string zStr = str.substr(8, (int)str.length() - 8);
+                                    z = std::stof(zStr.c_str(), nullptr);
+                                }
+                                
+                                if (strncmp(str.c_str(), "..NØ", 7) == 0)
+                                {
+                                    readingVerts = true;
+                                    getline(reader, str);
+                                }
+                            }
+                            
+                            if (strncmp(str.c_str(), ".SLUTT", 6) == 0)
+                            {
+                                stage = done;
+                                cout << "Converted " << curveCount << " curves." << endl;
+                                break;
+                            }
+                            
+                            if (readingVerts)
+                            {
+                                std:size_t splitPos = str.find(" ");
+                                string yStr = str.substr(0, (int)splitPos);
+                                string xStr = str.substr((int)splitPos + 1, (int)str.length() - (int)yStr.length());
+                                
+                                y = std::stof(yStr.c_str(), nullptr);
+                                x = std::stof(xStr.c_str(), nullptr);
+                                
+                                x *= unit;
+                                y *= unit;
+                                z *= unit;                
+                                
+                                writer << "(" << x << ", " << y << ", " << z << ") (0.5, 0.5, 0.5) (1, 0)" << endl;
+                            }
+                        }
+                        
+                        break;
+                    }
+                    case done:
+                    {
+                        break;
+                    }
                 }
             }
         }
         
         writer.close();
-        cout << "Converted " << curveIndex << " curves." << endl;
-    }
 }
 
 int main(int argc, char *argv[])
 {
-    //ConvertToVerticesTest("../Oblig1/SOSI/in.sos");
+    ConvertToVerticesTest("../Oblig1/SOSI/in.sos");
     
     ConvertToCurves("../Oblig1/SOSI/in.sos");
     
